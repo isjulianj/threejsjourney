d7227636ee852aeef67d293a3952794da324194c
julian-jackson <julian.jackson@frontierag.co.uk>
2025-05-21 15:30:42 +0100

    update scripts and simple html file.

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+export default {
+    // config options
+    server: {
+        port: 6969,
+        open: true,
+        // proxy: {
+        //     '/api': {
+        //         target: 'http://localhost:5000',
+        //         changeOrigin: true,
+        //         rewrite: (path) => path.replace(/^\/api/, '')
+        //     }
+        // }
+    }
+}
