e0797e05c3fe5cce1b500a11fcf8ea839e60ea53
julian-jackson <julian.jackson@frontierag.co.uk>
2025-05-22 16:30:38 +0100

    start with rotation

--- src/scripts.ts
+++ src/scripts.ts
@@ -3,16 +3,27 @@ import * as THREE from "three"
 // Canvas
 const canvas: HTMLCanvasElement = document.querySelector("#canvas-webgl")
 
+const axisHelper = new THREE.AxesHelper(3)
+
 // Scene
 const scene = new THREE.Scene()
 
 
 // Geometry
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-const cube = new THREE.Mesh(geometry, material)
+const material = new THREE.MeshBasicMaterial({ color: 0xffff00, opacity: 0.5 })
+const mesh = new THREE.Mesh(geometry, material)
 
-scene.add(cube)
+mesh.position.set(0.9, -0.6, 0.5)
+
+scene.add(mesh)
+scene.add(axisHelper)
+
+mesh.scale.set(2, 0.5, 1)
+
+// rotation
+mesh.rotation.y = Math.PI * 0.25
+mesh.rotation.x = Math.PI * 0.25
 
 const sizes = {
     width: 800,
@@ -27,7 +38,7 @@ const sizes = {
  * @see {@link https://threejs.org/docs/#api/en/cameras/PerspectiveCamera}
  */
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
-camera.position.z = 3
+camera.position.z = 6
 
 scene.add(camera)
 const renderer = new THREE.WebGLRenderer({
